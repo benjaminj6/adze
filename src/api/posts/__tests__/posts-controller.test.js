4159df8c3f84f6a1d69bb9b5349c813930a52bff
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-16 13:16:08 -0800

    Testing -- add tests for getLimitedPosts()

--- src/api/posts/__tests__/posts-controller.test.js
+++ src/api/posts/__tests__/posts-controller.test.js
@@ -56,10 +56,59 @@ describe('posts-controller', () => {
       mockQuery = sinon.mock(Post)
         .expects('find')
         .chain('sort').withArgs('-date')
+      next = sinon.spy()
     })
 
     afterEach(() => {
+      Post.find.restore()
+      ctx = {}
       mockQuery.reset()
+      next.reset()
+    })
+
+    it('should return amount of posts that user requests', async () => {
+      ctx.params = { number: 3 }
+
+      mockQuery
+        .chain('limit').withArgs(3)
+        .resolves(createPosts(3))
+
+      await controller.getLimitedPosts(ctx, next)
+      expect(ctx.status).to.equal(200)
+      expect(ctx.body).to.have.length(3)
+      expect(next.calledOnce).to.be.true
+    })
+
+    it('should throw if `ctx.params.number` is absent (400)', async () => {
+      ctx.app = { emit: () => {} }
+      ctx.params = {}
+      const spyEmitter = sinon.spy(ctx.app, 'emit')
+
+      mockQuery
+        .chain('limit').withArgs(3)
+        .resolves(createPosts(3))
+
+      await controller.getLimitedPosts(ctx, next)
+
+      expect(spyEmitter.calledOnce).to.be.true
+      expect(spyEmitter.args[0][1].status).to.equal(400)
+      expect(spyEmitter.args[0][1].message).to.equal('Posts must be a whole number above 0')
+    })
+
+    it('should throw if `ctx.params.number` is 0 (400)', async () => {
+      ctx.app = { emit: () => {} }
+      ctx.params = { number: 0 }
+      const spyEmitter = sinon.spy(ctx.app, 'emit')
+
+      mockQuery
+        .chain('limit').withArgs(3)
+        .resolves(createPosts(3))
+
+      await controller.getLimitedPosts(ctx, next)
+
+      expect(spyEmitter.calledOnce).to.be.true
+      expect(spyEmitter.args[0][1].status).to.equal(400)
+      expect(spyEmitter.args[0][1].message).to.equal('Posts must be a whole number above 0')
     })
   })
 })
