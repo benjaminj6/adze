e5552f77224bb678aa76818e954c165ea1d08db3
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-15 19:37:09 -0800

    Logger -- move to separate, reusable file

--- src/api/posts/posts-controller.js
+++ src/api/posts/posts-controller.js
@@ -1,3 +1,6 @@
+import { Post } from '../../models'
+import log from '../../config/logger'
+
 export function getPosts (ctx, next) {
   ctx.body = 'This will serve ALL of the posts in the db'
 }
@@ -10,8 +13,20 @@ export function deletePost (ctx, next) {
   ctx.body = `This will delete a post with the id ${ctx.params.id}`
 }
 
-export function addPost (ctx, next) {
-  ctx.body = 'This will add a post in the user\'s posts'
+export async function addPost (ctx, next) {
+  try {
+    const newPost = await Post.create({
+      title: [],
+      html: 'something else'
+    })
+
+    log.debug()
+    log.debug(`The html reads -- ${newPost.html}`)
+    ctx.body = 'This will add a post in the user\'s posts'
+  } catch (err) {
+    log.debug(`[ERR] ${err.name}: ${err.message}`)
+    next(err)
+  }
 }
 
 export function editPost (ctx, next) {
--- src/config/logger.js
+++ src/config/logger.js
@@ -1,7 +1,7 @@
 import bunyan from 'bunyan'
 
 // Setup logger instance
-const log = bunyan.createLogger({
+let log = bunyan.createLogger({
   level: process.env.LOG_LEVEL || 'info',
   name: process.env.NAME || 'blog-api'
 })
