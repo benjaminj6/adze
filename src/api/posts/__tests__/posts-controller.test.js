fabe38225623207a3c5fd8d941463994ba716aa1
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-16 12:54:56 -0800

    Testing -- add tests for error handing on getPosts()

--- src/api/posts/__tests__/posts-controller.test.js
+++ src/api/posts/__tests__/posts-controller.test.js
@@ -8,13 +8,13 @@ import { createPosts } from './test-utils'
 import * as controller from '../posts-controller'
 
 describe('posts-controller', () => {
-  describe('getPosts()', () => {
-    let ctx = {}
-    let next
-    let mock
+  let ctx = {}
+  let next
+  let mockQuery
 
+  describe('getPosts()', () => {
     beforeEach(() => {
-      mock = sinon.mock(Post)
+      mockQuery = sinon.mock(Post)
          .expects('find')
          .chain('sort').withArgs('-date')
       next = sinon.spy()
@@ -22,17 +22,44 @@ describe('posts-controller', () => {
 
     afterEach(() => {
       Post.find.restore()
+      ctx = {}
       next.reset()
-      mock.reset()
+      mockQuery.reset()
     })
 
     it('should return posts from the db in order by most recent', async () => {
-      mock.resolves(createPosts(5))
+      mockQuery.resolves(createPosts(5))
       await controller.getPosts(ctx, next)
 
       expect(ctx.status).to.equal(200)
       expect(ctx.body).to.have.length(5)
       expect(next.calledOnce).to.be.true
+    })
+
+    it('should throw with status 404', async () => {
+      ctx.app = { emit: () => {} }
+      const spyEmitter = sinon.spy(ctx.app, 'emit')
+
+      const testError = new Error('test error')
+      mockQuery.rejects(testError)
+
+      await controller.getPosts(ctx, next)
+
+      expect(spyEmitter.args[0][1].status).to.equal(404)
+      expect(spyEmitter.calledOnce).to.be.true
+      expect(spyEmitter.calledWith('error', testError, ctx)).to.be.true
+    })
+  })
+
+  describe('getLimitedPosts()', () => {
+    beforeEach(() => {
+      mockQuery = sinon.mock(Post)
+        .expects('find')
+        .chain('sort').withArgs('-date')
+    })
+
+    afterEach(() => {
+      mockQuery.reset()
     })
   })
 })
