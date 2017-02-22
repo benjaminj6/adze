33dda43f228d1c9ff04da781fbbd5a49ec8db40c
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-22 14:59:29 -0800

    Auth -- add endpoint to support logging out

--- src/api/auth/router/index.js
+++ src/api/auth/router/index.js
@@ -6,7 +6,11 @@ const auth = new Router()
 auth
   .post('/login', passport.authenticate('local', {
     successRedirect: '/api/posts/',
-    failureRedirect: '/api/posts/1'
+    failureRedirect: '/' // Will give anything right now...no file served from here
   }))
+  .get('/logout', (ctx, next) => {
+    ctx.logout()
+    ctx.redirect('/')
+  })
 
 export default auth
