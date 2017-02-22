694c3736cd4c5ae870f130c5d3d2a6e54290fd86
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-21 18:55:04 -0800

    Testing -- add tests for user model

---
+++ src/models/__tests__/user-model.test.js
@@ -0,0 +1,61 @@
+import test from 'ava'
+import { User } from '..'
+
+test('User -- should pass validation', t => {
+  const user = new User({
+    email: 'test@test.com',
+    password: 'test'
+  })
+
+  t.notThrows(user.validate())
+  t.is(user.email, 'test@test.com')
+  t.is(user.password, 'test')
+})
+
+async function errorTest (t, input) {
+  const user = new User(input)
+  const err = await t.throws(user.validate())
+  t.is(err.name, 'ValidationError')
+}
+
+test(
+  'User -- should fail validation if no email',
+  errorTest,
+  { password: 'test' }
+)
+
+test(
+  'User -- should fail validation if no password',
+  errorTest,
+  { email: 'test@test.com' }
+)
+
+test(
+  'User -- should fail validation if email has no `@`',
+  errorTest,
+  { email: 'testtesttest.com', password: 'test' }
+)
+
+test(
+  'User -- should fail validation if email has no `.TLD`',
+  errorTest,
+  { email: 'test@test', password: 'test' }
+)
+
+test(
+  'User -- should fail validation if email is not a string',
+  errorTest,
+  { email: [], password: 'test' }
+)
+
+test(
+  'User -- should fail validation if email is blank before `@`',
+  errorTest,
+  { email: '@test.com', password: 'test' }
+)
+
+test(
+  'User -- should fail validation if password is not a string',
+  errorTest,
+  { email: 'test@test.com', password: [] }
+)
