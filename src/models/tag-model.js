4d2d90b54372429e83ee5e0528c58b208495d4cd
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-23 13:57:36 -0800

    Tags -- add tag model

---
+++ src/models/tag-model.js
@@ -0,0 +1,20 @@
+import { isHexColor } from 'validator'
+import mongoose, { Schema } from 'mongoose'
+mongoose.Promise = global.Promise
+
+const TagSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    validate: {
+      validator: isHexColor,
+      message: 'Color must be a valid hexadecimal color'
+    }
+  }
+})
+
+const Tag = mongoose.model('Tag', TagSchema)
+export default Tag
