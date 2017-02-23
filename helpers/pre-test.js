003da847e2c91a9be3fde7358e7ed9812a81e7e3
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-22 16:42:25 -0800

    Testing -- Add pre test hook to check for a user in db

---
+++ helpers/pre-test.js
@@ -0,0 +1,33 @@
+require('dotenv').config({ path: '.env-test' })
+require('babel-register')
+require('babel-polyfill')
+
+const { User } = require('../src/models')
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+mongoose.connect(process.env.DB_URL)
+  .then(() => {
+    return User.findOne({ email: 'test@test.com' })
+      .then(user => user)
+  })
+  .then(user => {
+    if (!user) {
+      console.log('no user! adding one now...')
+      return User.hashPassword('test')
+        .then(password => {
+          return User.create({
+            email: 'test@test.com',
+            password: password
+          })
+        })
+    } else {
+      console.log('using the existing user')
+    }
+  })
+  .then(() => {
+    return mongoose.disconnect()
+  })
+  .catch(err => {
+    mongoose.disconnect()
+  })
