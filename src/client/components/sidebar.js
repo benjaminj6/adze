8d3e9371d4e216cef67237ae78e6106d8e59bf20
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-27 23:44:05 -0800

    Menu -- initial design

--- src/client/components/sidebar.js
+++ src/client/components/sidebar.js
@@ -1,9 +1,32 @@
 import { html } from '../utils'
 
 import search from './search'
+import { done } from './icons'
 
-export default (props = {}) => html`
-  <nav>
+const menuItem = (props) => html`
+  <li>
+    <a
+      href="${props.href}"
+      class="${props.class || ''}">
+      ${done()} ${props.content}
+    </a>
+  </li>
+`
+
+const test = {
+  nav: ['some really long sentance...what will happen if I do this', 'bar', 'baz']
+}
+
+export default (props = test) => html`
+  <nav id="sidebar">
     ${search()}
+    <ul>
+      ${
+        props.nav.map(link => menuItem({
+          href: link,
+          content: link
+        }))
+      }
+    </ul>
   </nav>
 `
