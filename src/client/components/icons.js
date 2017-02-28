46eb96a0d3460aede2850d3170cc4fa8998ebf33
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-28 09:27:46 -0800

    Header -- add `more` icon

--- src/client/components/icons.js
+++ src/client/components/icons.js
@@ -3,7 +3,9 @@ import { html } from '../utils'
 const defaultIconProps = {
   fill: '#000',
   height: '24',
-  width: '24'
+  width: '24',
+  viewBox: '0 0 24 24',
+  xmlns: 'http://www.w3.org/2000/svg'
 }
 
 export const done = (props = defaultIconProps) => html`
@@ -11,9 +13,9 @@ export const done = (props = defaultIconProps) => html`
     class="done"
     fill=${props.fill}
     height=${props.height}
-    viewBox="0 0 24 24"
+    viewBox=${props.viewBox}
     width=${props.width}
-    xmlns="http://www.w3.org/2000/svg">
+    xmlns=${props.xmlns}>
     <path d="M0 0h24v24H0z" fill="none" />
     <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
   </svg>
@@ -24,10 +26,23 @@ export const menu = (props = defaultIconProps) => html`
     class="menu"
     fill=${props.fill}
     height=${props.height}
-    viewBox="0 0 24 24"
+    viewBox=${props.viewBox}
     width=${props.height}
-    xmlns="http://www.w3.org/2000/svg">
+    xmlns=${props.xmlns}>
     <path d="M0 0h24v24H0z" fill="none"/>
     <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+  </svg>
+`
+
+export const moreVert = (props = defaultIconProps) => html`
+  <svg
+    class="more-vertical"
+    fill=${props.fill}
+    height=${props.height}
+    viewBox=${props.viewBox}
+    width=${props.height}
+    xmlns=${props.xmlns}>
+      <path d="M0 0h24v24H0z" fill="none"/>
+      <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
   </svg>
 `
