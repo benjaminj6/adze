c09d723cdc7ad2484f1aea0f359c6995c5febe5f
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-28 10:26:21 -0800

    Header -- try three icons for placement

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -2,13 +2,19 @@ import { html } from '../utils'
 import { menu, moreVert } from './icons'
 import button from './button'
 
+const buttons = ['a', '2', '4']
+
 export default () => html`
   <header>
     ${button({
       content: menu()
     })}
-    ${button({
-      content: moreVert()
-    })}
+    <div class="settings">
+      ${buttons.map(btn => button(
+        {
+          content: moreVert()
+        }
+      ))}
+    </div>
   </header>
 `
