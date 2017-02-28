814d780584286e178fb0939ed74441825e4859ee
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-28 15:50:02 -0800

    Icons -- formatting

--- src/client/components/icons.js
+++ src/client/components/icons.js
@@ -1,9 +1,8 @@
-/* eslint-disable */
 import { h } from 'hyperapp' //eslint-disable-line
 
 const Svg = (props = {}, children) =>
   <svg
-    className="done"
+    className='done'
     fill={props.fill || '#000'}
     height={props.height || 24}
     width={props.width || 24}
@@ -13,27 +12,27 @@ const Svg = (props = {}, children) =>
 
 export const Ex = props =>
   <Svg {...props}>
-    <path d="M0 0h24v24H0V0z" fill="none"/>
-    <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"/>
+    <path d='M0 0h24v24H0V0z' fill='none' />
+    <path d='M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z' />
   </Svg>
 
 export const Menu = props =>
   <Svg {...props}>
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' />
   </Svg>
 
 export const MoreVert = props =>
   <Svg {...props}>
-      <path d="M0 0h24v24H0z" fill="none"/>
-      <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z' />
   </Svg>
 
 export const Tag = props =>
   <Svg
     viewBox='-2 -2 28 28'
     {...props}>
-      <path d="M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.6,7M21.41,11.58L12.41,2.58C12.05,2.22 11.55,2 11,2H4C2.89,2 2,2.89 2.3,4V11C2,11.55 2.22,12.05 2.59,12.41L11.58,21.41C11.95,21.77 12.45,22 13,22C13.55,22 14.05,21.77 14.41,21.41L21.41,14.41C21.78,14.05 22,13.55 22,13C22,12.44 21.77,11.94 21.41,11.58Z" />
+    <path d='M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.6,7M21.41,11.58L12.41,2.58C12.05,2.22 11.55,2 11,2H4C2.89,2 2,2.89 2.3,4V11C2,11.55 2.22,12.05 2.59,12.41L11.58,21.41C11.95,21.77 12.45,22 13,22C13.55,22 14.05,21.77 14.41,21.41L21.41,14.41C21.78,14.05 22,13.55 22,13C22,12.44 21.77,11.94 21.41,11.58Z' />
   </Svg>
 
 export const Save = props =>
@@ -41,12 +40,12 @@ export const Save = props =>
     className='icon-save'
     viewBox='-2 -2 28 28'
     {...props}>
-      <path d="M0 0h24v24H0z" fill="none"/>
-      <path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/>
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z' />
   </Svg>
 
 export const Info = props =>
   <Svg {...props}>
-    <path d="M0 0h24v24H0z" fill="none"/>
-    <path d="M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z"/>
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' />
   </Svg>
