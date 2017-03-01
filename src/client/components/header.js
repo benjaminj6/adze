d428c365b383597b85460474e962ed3bf0184e69
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-28 16:06:50 -0800

    Formatting -- no changes

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,20 +1,23 @@
 import { h } from 'hyperapp' // eslint-disable-line
 
 import Button from './Button'
-import { Menu, MoreVert, Save } from './Icons' // eslint-disable-line
+import { Menu, Info, Save } from './Icons' // eslint-disable-line
+
+const Settings = ({ buttons }) =>
+  <div className='settings'>
+    {buttons &&
+      buttons.map(btn => <Button style={btn.style}>{btn.content}</Button>)
+    }
+  </div>
 
 export default ({ buttons }) =>
   <header>
     <Button>
       <Menu />
     </Button>
-    <div class='settings'>
-      {buttons &&
-        buttons.map(({ style, content }) =>
-          <Button style={style}>
-            {content}
-          </Button>
-        )
-      }
-    </div>
+    <Settings
+      buttons={[
+        { content: <Save /> },
+        { content: <Info /> }
+      ]} />
   </header>
