9221942fabaa00a71cf184148088c47b459a7357
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-03-10 11:49:18 -0800

    Login page (#40)
    * (#32) Basic styling of the login
    
    * (#30) Fix bottom input border radius
    
    * (#30) Add demo
    
    * (#30) Link up <a> tag
    
    * (#30) Hook up demo and sign in to the server

--- src/client/view/pages/LoginView.js
+++ src/client/view/pages/LoginView.js
@@ -1,34 +1,65 @@
 import { h } from 'hyperapp' // eslint-disable-line
 
 export default (model, actions) => (
-  <div id='app'>
-    <form action='/api/auth/login' method='POST' onsubmit={ev => {
-      console.log(ev)
-      ev.preventDefault()
-      window.fetch('/api/auth/login', {
-        method: 'POST',
-        body: JSON.stringify({
-          email: 'test@test.com',
-          password: 'test'
-        }),
-        headers: new window.Headers({
-          'Content-Type': 'application/json'
-        })
-      }).then(res => {
-        console.log(res)
-        if (res.status !== 200) {
-          throw new Error('Unauthorized')
-        }
-
-        actions.router.go('/dashboard')
-      }).catch(err => {
-        console.log('there was a fatal error')
-        console.log(err)
-      })
-    }}>
-      <input type='email' name='email' id='login-username' />
-      <input type='password' name='password' />
-      <button type='submit'>Submit</button>
+  <div
+    id='app'
+    className='login-view'>
+    <form
+      action='/api/auth/login'
+      method='POST'
+      onsubmit={login(undefined, actions)}>
+      <h2 style={{
+        margin: '1rem 0'
+      }}>{process.env.NAME}</h2>
+      <input
+        type='email'
+        name='email'
+        id='login-username'
+        placeholder='email' />
+      <input
+        type='password'
+        name='password'
+        placeholder='password'
+       />
+      <button type='submit'>Log in</button>
+      {
+        process.env.DEMO
+        ? <a
+          href='/dashboard'
+          onclick={login({
+            email: process.env.ADMIN_EMAIL,
+            password: process.env.PASSWORD
+          }, actions)}>See the demo</a>
+        : ''
+      }
     </form>
   </div>
 )
+
+function login (data = {}, actions) {
+  return ev => {
+    ev.preventDefault()
+    console.log(ev)
+    console.log(ev.target.email, ev.target.password)
+    window.fetch('/api/auth/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: data.email || ev.target.querySelector('[name=email]').value,
+        password: data.password || ev.target.querySelector('[name=password]').value
+      }),
+      headers: new window.Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(res => {
+      console.log(res)
+      if (res.status !== 200) {
+        throw new Error('Unauthorized')
+      }
+
+      actions.router.go('/dashboard')
+    }).catch(err => {
+      console.log('there was a fatal error')
+      console.log(err)
+    })
+  }
+}
