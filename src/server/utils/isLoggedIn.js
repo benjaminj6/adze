4a2b75eeb18888eb9e19fb1fe8f2adb365274317
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-03-11 13:06:37 -0800

    (#38) Remove loggers -- no changes

--- src/server/utils/isLoggedIn.js
+++ src/server/utils/isLoggedIn.js
@@ -1,10 +1,6 @@
 import createError from './errorGenerator' // eslint-disable-line
 
 export default (ctx, next) => {
-  // console.log('req', ctx.request)
-  // console.log('pp', ctx.passport)
-  // console.log('authenticated?', ctx.state)
-  // console.log('ctx', ctx)
   if (ctx.passport.user) {
     return next()
   }
