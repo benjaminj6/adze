e902b9f589d22aa952d53978da1a5b785047be56
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-03-13 00:53:21 -0700

    (#46) PromptView.js

--- src/client/view/pages/DashboardView.js
+++ src/client/view/pages/DashboardView.js
@@ -1,27 +1,9 @@
 import { h } from 'hyperapp' // eslint-disable-line
 
 import EditorView from '../components/EditorView'
+import PromptView from '../components/PromptView'
 import TagsView from '../components/TagsView'
-
 import Sidebar from '../components/Sidebar'
-
-const PromptView = ({ model, actions }) => (
-  <main>
-    <header />
-    <section className='prompt'>
-      <h2>
-        You can select posts and categories on the left.
-        <br />
-        Or you can start a <a
-          href='/dashboard/create'
-          onclick={ev => {
-            ev.preventDefault()
-            actions.router.go('/dashboard/create')
-          }}>new one today</a>.
-      </h2>
-    </section>
-  </main>
-)
 
 export default (model, actions) => {
   return (
