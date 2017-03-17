93ea8eeecf321404495dcd9043889dfab7b7283e
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-03-17 00:02:47 -0700

    Update deploy script

--- helpers/prod.js
+++ helpers/prod.js
@@ -1,4 +1,27 @@
 require('dotenv').config()
 const { start } = require('../prod')
+const { User } = require('../prod/models')
 
-start()
+mongoose.connect(process.env.DB_URL)
+  .then(() => {
+    return User.findOne({
+      email: process.env.ADMIN_EMAIL
+    }).then(user => user)
+  })
+  .then(user => {
+    if (!user) {
+      return User.hashPassword(process.env.PASSWORD)
+        .then(password => {
+          return User.create({
+            email: process.env.ADMIN_EMAIL,
+            password
+          })
+        })
+    }
+    .then(() => {
+      return mongoose.disconnect()
+    })
+    .then(() => {
+      start()
+    })
+  })
