ec3649f30c6a1a64e411a3e7206a75adcf73aff8
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-14 08:46:01 -0700

    deploy: refactor login function again

--- src/client/view/Login/index.js
+++ src/client/view/Login/index.js
@@ -16,7 +16,13 @@ export default (model, actions) => (
     <form
       action='/api/auth/login'
       method='POST'
-      onsubmit={login(undefined, actions)}>
+      onsubmit={ev => {
+        ev.preventDefault()
+        login({
+          email: ev.target.querySelector('[name=email]').value,
+          password: ev.target.querySelector('[name=password]').value
+        }, actions)
+      }}>
       <h2 style={{
         margin: '1rem 0'
       }}>{process.env.NAME}</h2>
@@ -52,8 +58,8 @@ export function login (data = {}, actions) {
   window.fetch('/api/auth/login', {
     method: 'POST',
     body: JSON.stringify({
-      email: data.email || ev.target.querySelector('[name=email]').value,
-      password: data.password || ev.target.querySelector('[name=password]').value
+      email: data.email,
+      password: data.password
     }),
     headers: new window.Headers({
       'Content-Type': 'application/json'
