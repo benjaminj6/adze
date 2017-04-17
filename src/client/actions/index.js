7213f19e10ab646d0748a447c41c843db9bbaae4
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-17 12:20:43 -0700

    cleanup: organize actions export file

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,4 +1,4 @@
-// Staging Posts
+// Posts (staging)
 export { default as addStagedTag } from './addStagedTag'
 export { default as removeStagedTag } from './removeStagedTag'
 export { default as updateStagedPost } from './updateStagedPost'
@@ -6,15 +6,23 @@ export { default as updateStagedTitle } from './updateStagedTitle'
 export { default as selectPost } from './selectPost'
 export { default as clearNewContent } from './clearNewContent'
 
-// Modify `model.posts`
+// Posts (model)
 export { default as addPost } from './addPost'
 export { default as addAllPosts } from './addAllPosts'
 export { default as updatePost } from './updatePost'
 export { default as deletePostFromModel } from './deletePostFromModel'
 
-// Modify `model.tags`
+// Tags (staging)
+export { default as stageTagName } from './stageTagName'
+export { default as stageTagColor } from './stageTagColor'
+export { default as clearStagedTag } from './clearStagedTag'
+export { default as selectTag } from './selectTag'
+
+// Tags (model)
 export { default as addTag } from './addTag'
 export { default as addAllTags } from './addAllTags'
+export { default as removeTagFromModel } from './removeTagFromModel'
+export { default as updateSavedTag } from './updateSavedTag'
 
 // Write to database
 export { default as saveNewPost } from './saveNewPost'
@@ -23,11 +31,3 @@ export { default as deletePost } from './deletePost'
 export { default as logout } from './logout'
 export { default as saveTag } from './saveTag'
 export { default as deleteTag } from './deleteTag.js'
-
-// Tags
-export { default as stageTagName } from './stageTagName'
-export { default as stageTagColor } from './stageTagColor'
-export { default as clearStagedTag } from './clearStagedTag'
-export { default as selectTag } from './selectTag'
-export { default as updateSavedTag } from './updateSavedTag'
-export { default as removeTagFromModel } from './removeTagFromModel'
