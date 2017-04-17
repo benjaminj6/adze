346b803d155ddcaaf7783df6fb3cd24d9e70d90e
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-17 14:34:08 -0700

    cleanup: move `loadRemoteData` into separate file

---
+++ src/client/subscriptions/loadRemoteData.js
@@ -0,0 +1,39 @@
+export const getPosts = (model, actions) => {
+  return window.fetch('/api/posts')
+    .then(res => {
+      return res.json()
+    })
+    .then(json => {
+      actions.addAllPosts(json)
+    })
+    .catch(err => console.error('this handler', err))
+}
+
+export const getTags = (model, actions) => {
+  return window.fetch('/api/tags')
+    .then(res => res.json())
+    .then(json => {
+      actions.addAllTags(json)
+    })
+    .catch(err => console.error(err))
+}
+
+const getInitialNewContent = (model, actions) => {
+  if (/create|tags/.test(model.router.match)) {
+    actions.clearNewContent()
+  }
+
+  if (/tags/.test(model.router.match)) {
+    actions.selectTag(model.router.params.id)
+  }
+
+  if (/posts/.test(model.router.match)) {
+    actions.selectPost(model.router.params.id)
+  }
+}
+
+export default (model, actions) => {
+  getPosts(model, actions)
+    .then(_ => getTags(model, actions))
+    .then(_ => getInitialNewContent(model, actions))
+}
