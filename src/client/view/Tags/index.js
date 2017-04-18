a3b24641d5f27da278c1df5cdd3603d8ae5f8a19
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-18 10:37:55 -0700

    cleanup: Tags

--- src/client/view/Tags/index.js
+++ src/client/view/Tags/index.js
@@ -4,18 +4,15 @@ import debounce from 'lodash.debounce'
 import PostCard from './PostCard'
 import InputHeader from '../general/InputHeader'
 import HeaderButtons from '../general/HeaderButtons'
-
 import { Trash, Save, SaveCheck, Paint } from '../general/Icons'
+
 import styles from '../../styles/foundation.json'
 
 const getTagHeaderButtons = (model, actions) => {
   const savable = model.newTagData.name
   const saveButton = (savable && model.newTagDataSaved)
     ? <SaveCheck />
-    : <Save
-      style={{
-        color: !savable ? 'rgba(0, 0, 0, 0.1)' : ''
-      }} />
+    : <Save style={{color: !savable ? 'rgba(0, 0, 0, 0.1)' : ''}} />
 
   return [
     <label htmlFor='edit-tag-submit'>{saveButton}</label>,
@@ -28,65 +25,59 @@ export default ({ model, actions, tag }) => {
   let posts = []
 
   if (tag) {
-    posts = model.posts.filter(p => {
-      return p.tags.find(t => t._id === tag._id)
-    })
+    posts = model.posts.filter(p => p.tags.find(t => t._id === tag._id))
   }
 
   const buttons = getTagHeaderButtons(model, actions)
+
+  // Event handlers
+  const debounceInput = debounce(({ target }) => actions.stageTagName(target.value), 300)
+  const formSubmitHandler = ev => {
+    ev.preventDefault()
+    if (!model.newTagDataSaved) {
+      actions.saveTag({ ...model.newTagData })
+    }
+  }
 
   return (
     <main>
       <header className='tags-header'>
         <HeaderButtons buttons={buttons} />
       </header>
-      {/* TODO: move into .scss */}
-      <section className='tags-section' style={{
-        padding: '3rem 5%'
-      }}>
+
+      <section className='tags-section'>
         <header>
           <form
             id='edit-tag'
-            oncreate={el => {
-              console.log('created')
-            }}
-            onremove={el => {
-              console.log('removed')
-            }}
-            onsubmit={ev => {
-              ev.preventDefault()
-              if (!model.newTagDataSaved) {
-                actions.saveTag({ ...model.newTagData })
-              }
-            }}>
+            onsubmit={formSubmitHandler}>
             <InputHeader
               className='tag-input-header'
               name='name'
+              oninput={debounceInput}
               placeholder='tag name'
-              value={tag ? tag.name : ''}
-              oninput={
-                debounce(({ target }) => {
-                  console.log('this runs')
-                  actions.stageTagName(target.value)
-                }, 300)
-              } />
+              value={tag ? tag.name : ''} />
+
             <input
+              defaultValue={styles['accent-lighter']}
               hidden
               id='edit-tag-color'
               name='color'
-              defaultValue={styles['accent-lighter']}
               type='color' />
+
             <input
               hidden
               id='edit-tag-submit'
               type='submit' />
           </form>
-          <h6>({
-            posts.length === 1
-            ? `${posts.length} post`
-            : `${posts.length} posts`
-          })</h6>
+          <h6>
+            ({
+              posts.length === 1
+                ? `${posts.length} post`
+                : `${posts.length} posts`
+            })
+          </h6>
         </header>
+
         <section className='posts'>
           <ul>
             {posts.map(p => <PostCard post={p} actions={actions} />)}
